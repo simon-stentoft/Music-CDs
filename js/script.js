174505a767af88f53a0e845252a7ae563e92a0c1
Simon <57576661+simon-stentoft@users.noreply.github.com>
2025-02-20 12:14:41 +0100

    Rewrote code to include tables instead of template to more like the example

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const addForm = document.getElementById("addForm");
-const cdTemplate = document.querySelector("template").content;
-const main = document.querySelector("main");
+const cdTable = document.getElementById("cdTable").querySelector("tbody");
 
 addForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,15 +7,18 @@ addForm.addEventListener("submit", (e) => {
   const title = document.getElementById("title").value;
   const year = document.getElementById("year").value;
 
-  const clone = cdTemplate.cloneNode(true);
-  clone.querySelector("h2:nth-of-type(1)").textContent = `Author: ${author}`;
-  clone.querySelector("h2:nth-of-type(2)").textContent = `Title: ${title}`;
-  clone.querySelector("h2:nth-of-type(3)").textContent = `Year: ${year}`;
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>${author}</td>
+    <td>${title}</td>
+    <td>${year}</td>
+    <td><button class="deleteBtn"></button></td>
+  `;
 
-  clone.querySelector(".deleteBtn").addEventListener("click", (e) => {
-    e.target.closest(".cd").remove();
+  row.querySelector(".deleteBtn").addEventListener("click", (e) => {
+    e.target.closest("tr").remove();
   });
 
-  main.appendChild(clone);
+  cdTable.appendChild(row);
   addForm.reset();
 });
