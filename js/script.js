ac2a7b93876d206de3ea1228ef81c1c6f97b19e7
Simon <57576661+simon-stentoft@users.noreply.github.com>
2025-02-26 15:31:56 +0100

    fixed use of xhtml. removed field tags made into divs. moved all colors into variables. redid some javascript by using e.target.name.value to make it abit more efficient. made addeventlistener into a normal function and not arrow function, so i can use this.reset(). moved const cdtable into the eventlistener so it only loads with button press

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 const addForm = document.getElementById("addForm");
-const cdTable = document.getElementById("cdTable").querySelector("tbody");
 
-addForm.addEventListener("submit", (e) => {
+addForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  const author = document.getElementById("author").value;
-  const title = document.getElementById("title").value;
-  const year = document.getElementById("year").value;
+
+  // The form inputs in the HTML need name attributes to be able to access their values
+
+  /* Using this method instead of e.target
+  const author = this.author.value;
+  const title = this.title.value;
+  const year = this.year.value;
+  */
+  const author = e.target.author.value;
+  const title = e.target.title.value;
+  const year = e.target.year.value;
 
   const row = document.createElement("tr");
   row.innerHTML = `
@@ -15,10 +22,13 @@ addForm.addEventListener("submit", (e) => {
     <td><button class="deleteBtn"></button></td>
   `;
 
-  row.querySelector(".deleteBtn").addEventListener("click", (e) => {
+  row.querySelector(".deleteBtn").addEventListener("click", function (e) {
     e.target.closest("tr").remove();
   });
 
+  const cdTable = document.getElementById("cdTable");
   cdTable.appendChild(row);
-  addForm.reset();
+
+  // Reset the form after submission
+  this.reset();
 });
